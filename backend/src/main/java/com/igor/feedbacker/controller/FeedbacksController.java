89abf75d408cd4f0776bc7148ad045487bd305a7
IgorRibeiro-S <igor_rsilva97@hotmail.com>
2023-04-30 16:45:43 -0300

    update

--- backend/src/main/java/com/igor/feedbacker/controller/FeedbacksController.java
+++ backend/src/main/java/com/igor/feedbacker/controller/FeedbacksController.java
@@ -114,9 +114,10 @@ public class FeedbacksController {
 		return ResponseEntity.noContent().build();
 	}
 
+	@ApiOperation(value = "", authorizations = { @Authorization(value = "Bearer") })
 	@PostMapping
 	public ResponseEntity<Feedbacks> newFeedback(@RequestBody Feedbacks obj) {
-		obj.setIdUser(repo.findByApiKey(obj.getApiKey()).getId());
+		obj.setIdUser(obj.getUser().getId());
 		Users us1 = service.findById(obj.getUser().getId());
 		obj.setApiKey(us1.getApiKey());
 		Feedbacks feedbacks1 = feedbackssService.novoFeedback(obj);
